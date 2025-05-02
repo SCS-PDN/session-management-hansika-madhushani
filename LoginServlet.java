44ba11f51be3f48d64fb2d9c4108c261ee3fd30d
hansika-madhushani <s20440@sci.pdn.ac.lk>
2025-05-02 15:29:35 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,19 +1,33 @@
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        // TODO: Implement login logic
-        // 1. Get username & password from request
-        // 2. Validate credentials (hardcode a few users)
-        // 3. If valid:
-        //    - Create session
-        //    - Store username in cookie
-        //    - Redirect to DashboardServlet
-        // 4. If invalid, redirect back to login.html
+    private Map<String, String> users = Map.of("student1", "pass1", "student2", "pass2");
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            Cookie userCookie = new Cookie("username", username);
+            userCookie.setMaxAge(60*60); // 1 hour
+            response.addCookie(userCookie);
+
+            response.sendRedirect("DashboardServlet");
+        } else {
+            response.getWriter().println("Invalid credentials. Try again.");
+        }
     }
 }
